e278f1425a518e3ed36e38fa6fb23d39ce4da9fc
Ben Pepin <ben.a.pepin@gmail.com>
2017-03-14 20:58:07 -0400

    stuffappearsnow

--- src/App.js
+++ src/App.js
@@ -2,35 +2,56 @@ import React, { Component } from 'react';
 import './App.css';
 import Piece from './Piece.js';
 
+const projects = [
+  {
+    src: "https://static1.squarespace.com/static/55b3be84e4b07dd5b896634d/t/56f9a473b654f9c06ed434e4/1459201446427/",
+  },
+  {
+    src: "https://static1.squarespace.com/static/55b3be84e4b07dd5b896634d/56cbe85ecf80a1fc057b688b/56cbe860c2ea518471deaf86/1456203891649/Discover.png?format=300w",
+  },
+  {
+    src: "http://placecorgi.com/250",
+  },
+]
+
+function pieceFromProject(project) {
+  return (<Piece
+    imageSrc={project.src}
+    projectLink="http://www.benpepin.com/comixtoon"
+    isShowing={true}
+  />)
+}
+
 class App extends Component {
 
   constructor(props) {
     super(props);
+
     this.state = { numberShowing: 1 };
   }
 
   showAdditional() {
-    console.log("showing addtiional")
-    console.log(this)
     this.setState({
       numberShowing: this.state.numberShowing + 1
-    })
+    });
   }
+
+  showProjects() {
+    console.log("showing projects")
+    console.log(this)
+    return (
+      projects.slice(0, this.state.numberShowing).map((project) => pieceFromProject(project))
+    )
+  }
+
   render() {
     return (
 
       <div className="App" >
         <h1 onClick={this.showAdditional.bind(this)}>Ben Designed This...</h1>
-        <Piece
-          imageSrc="https://static1.squarespace.com/static/55b3be84e4b07dd5b896634d/t/56f9a473b654f9c06ed434e4/1459201446427/"
-          projectLink="http://www.benpepin.com/comixtoon"
-          isShowing={true}
-        />
-        <Piece
-          imageSrc="https://static1.squarespace.com/static/55b3be84e4b07dd5b896634d/56cbe85ecf80a1fc057b688b/56cbe860c2ea518471deaf86/1456203891649/Discover.png?format=300w"
-          projectLink="http://www.benpepin.com"
-          isShowing={false}
-        />
+        <div className="projects">
+          {this.showProjects()}
+        </div>
         <h1>{this.state.numberShowing}</h1>
       </div>
 
